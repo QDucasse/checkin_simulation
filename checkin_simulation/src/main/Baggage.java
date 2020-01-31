7f96c45a0001ef5feb99189cbf1956b7e62bb2b5
Thomas Thibault <thibault.thomas@epitech.eu>
2020-01-31 14:38:11 +0000

    Add Baggage.java

--- checkin_simulation/src/main/Baggage.java
+++ checkin_simulation/src/main/Baggage.java
@@ -1,4 +1,32 @@
 package main;
 
 public class Baggage {
+
+    private final int length;
+    private final int height;
+    private final int width;
+    private final int weight;
+
+    public Baggage(int length, int height, int width, int weight) {
+        this.length = length;
+        this.height = height;
+        this.width = width;
+        this.weight = weight;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
