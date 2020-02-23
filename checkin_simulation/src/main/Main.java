79acacd36823ff43afa856e710a52f9dd4f1cca6
Estelle <ed71@hw.ac.uk>
2020-02-23 13:02:07 +0000

    GUI call and tests

--- checkin_simulation/src/main/Main.java
+++ checkin_simulation/src/main/Main.java
@@ -1,4 +1,16 @@
 package main;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Airport airport = new Airport();
+        Flight f1 = new Flight("destination", "carrier", 8, 100, 100, "EH145");
+        Passenger passenger = new Passenger(airport, "name", "EH145", "1225", true);
+        Passenger passenger2 = new Passenger(airport, "joan", "EH146", "1244", false);
+        f1.addPassenger(passenger);
+        f1.addPassenger(passenger2);
+        f1.showGUI();
+    }
+
 }
