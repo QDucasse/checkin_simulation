e980d53e37f191f02ef514d20d769c401978ab5f
QDucasse <quentin.ducasse@ensta-bretagne.org>
2020-02-25 15:29:23 +0000

    Airport tests

--- checkin_simulation/src/main/Main.java
+++ checkin_simulation/src/main/Main.java
@@ -3,21 +3,17 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-
         Airport airport = new Airport();
         Flight f1 = new Flight("destination", "carrier", 8, 100, 100, "EH145");
         Passenger passenger1 = new Passenger("name", "EH145", "1225", true);
-        Passenger passenger2 = new Passenger("joan", "EH146", "1244", false);
+        Passenger passenger2 = new Passenger("joan", "EH145", "1244", true);
         Baggage baggage1 = new Baggage(10, 20, 30, 9);
         Baggage baggage2 = new Baggage(20, 10, 30, 11);
         passenger1.setBaggage(baggage1);
         passenger2.setBaggage(baggage2);
         f1.addPassenger(passenger1);
         f1.addPassenger(passenger2);
-        System.out.println(f1.totalWeight());
-        System.out.println(f1.totalVolume());
-        System.out.println(f1.totalPassengers());
-        //f1.showGUI();
+        f1.showGUI();
     }
 
 }
--- checkin_simulation/src/test/AirportTest.java
+++ checkin_simulation/src/test/AirportTest.java
@@ -1,28 +1,87 @@
 package test;
 
+import junit.framework.TestCase;
+import main.Airport;
+import main.Baggage;
+import main.Flight;
+import main.Passenger;
+import main.exceptions.FlightNotFoundException;
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 
-public class AirportTest {
-    @BeforeClass
-    public void setupBeforeClass() {
-
-    }
-
-    @AfterClass
-    public void tearDownAfterClass() {
-
-    }
+public class AirportTest extends TestCase {
+    private Airport dummyAirport;
+    private Baggage dummyBaggage1;
+    private Baggage dummyBaggage2;
+    private Baggage dummyBaggage3;
+    private Passenger dummyPassenger1;
+    private Passenger dummyPassenger2;
+    private Passenger dummyPassenger3;
+    private Flight dummyFlight1;
+    private Flight dummyFlight2;
 
     @Before
     public void setUp() {
+        dummyBaggage1 = new Baggage(10, 20, 30, 9);
+        dummyPassenger1 = new Passenger("John Doe", "EH145", "1225", true);
+        dummyPassenger1.setBaggage(dummyBaggage1);
+
+        dummyBaggage2 = new Baggage(40, 40, 40, 30);
+        dummyPassenger2 = new Passenger( "Jane Doe", "FR145", "1226", false);
+        dummyPassenger2.setBaggage(dummyBaggage2);
+
+        dummyBaggage3 = new Baggage(10, 20, 30, 80);
+        dummyPassenger3 = new Passenger( "Bill Murray", "FR145", "1227", false);
+        dummyPassenger3.setBaggage(dummyBaggage3);
+
+        dummyFlight1 = new Flight("Edinburgh", "RyanAir", 2, 100, 100, "EH145");
+        dummyFlight2 = new Flight("Paris", "RyanAir", 2, 100, 100, "FR145");
+
+        dummyFlight1.addPassenger(dummyPassenger1);
+        dummyFlight2.addPassenger(dummyPassenger2);
+        dummyFlight2.addPassenger(dummyPassenger3);
+
+        dummyAirport = new Airport();
+        dummyAirport.addPassenger(dummyPassenger1);
+        dummyAirport.addPassenger(dummyPassenger2);
+        dummyAirport.addFlight(dummyFlight1);
+        dummyAirport.addFlight(dummyFlight2);
     }
 
-    @After
-    public void tearDown() {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
+    @Test
+    public void testGetFlightRefFound() throws FlightNotFoundException {
+        assertEquals(dummyFlight2,dummyAirport.getFlightFromRef("FR145"));
     }
 
     @Test
-    void testMonitor() {
+    public void testGetFlightRefNotFound() throws FlightNotFoundException {
+        try{
+            dummyAirport.getFlightFromRef("AA000");
+        } catch(FlightNotFoundException e){
+            assertEquals("Flight reference not found",e.getMessage());
+        }
+    }
+
+    @Test
+    public void testReport() {
+        String expected = "========================\n" +
+                "Report for flight EH145: \n" +
+                "Number of passengers: 1/2\n" +
+                "Weight: 9/100\n" +
+                "Volume: 6/100\n" +
+                "\n" +
+                "========================\n" +
+                "========================\n" +
+                "Report for flight FR145: \n" +
+                "Number of passengers: 2/2\n" +
+                "Weight: 110/100\n" +
+                "Volume: 70/100\n" +
+                "\n" +
+                "The weight is exceeded!\n" +
+                "========================\n";
+        assertEquals(expected,dummyAirport.outputReport());
     }
 }
