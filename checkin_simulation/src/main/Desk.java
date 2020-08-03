b24bdce48fce64a0c797270c28dc4be603a6e1dc
stouanen <stouanen@yahoo.fr>
2020-08-03 02:04:41 +0200

    Null pointer exeptions on null passenger/try to remove passenger from empty queue

--- checkin_simulation/src/main/Desk.java
+++ checkin_simulation/src/main/Desk.java
@@ -1,4 +1,5 @@
 package main;
+
 import main.exceptions.FlightNotFoundException;
 import main.exceptions.NegativeDimensionException;
 import main.exceptions.NullDimensionException;
@@ -7,163 +8,166 @@ import java.util.Observable;
 import java.util.Random;
 
 public class Desk extends Observable implements Runnable {
-    /* =======================
-        INSTANCE VARIABLES
-	======================= */
-    private PassengerQueue passengerQueue;
-    private Passenger currentPassenger = null;
-    private int processingTime;
-    private int deskNumber;
-    private Airport airport;
+	/*
+	 * ======================= INSTANCE VARIABLES =======================
+	 */
+	private PassengerQueue passengerQueue;
+	private Passenger currentPassenger = null;
+	private int processingTime;
+	private int deskNumber;
+	private Airport airport;
 
-    /* =======================
-          CONSTRUCTORS
-	======================= */
+	/*
+	 * ======================= CONSTRUCTORS =======================
+	 */
 
-    /**
-     * The desk is a central element to go through the check-in of the passengers.
-     * @param passengerQueue
-     *      The queue holding all the passengers waiting to check-in.
-     * @param processingTime
-     *      The time this desk takes to process one passenger.
-     */
-    public Desk(Airport airport, PassengerQueue passengerQueue, int processingTime, int deskNumber){
-        this.airport= airport;
-        this.passengerQueue = passengerQueue;
-        this.processingTime = processingTime;
-        this.deskNumber = deskNumber;
-    }
+	/**
+	 * The desk is a central element to go through the check-in of the passengers.
+	 * 
+	 * @param passengerQueue The queue holding all the passengers waiting to
+	 *                       check-in.
+	 * @param processingTime The time this desk takes to process one passenger.
+	 */
+	public Desk(Airport airport, PassengerQueue passengerQueue, int processingTime, int deskNumber) {
+		this.airport = airport;
+		this.passengerQueue = passengerQueue;
+		this.processingTime = processingTime;
+		this.deskNumber = deskNumber;
+	}
 
-    public Desk(Airport airport, PassengerQueue passengerQueue, int deskNumber){
-        this(airport, passengerQueue, 3000, deskNumber);
-    }
+	public Desk(Airport airport, PassengerQueue passengerQueue, int deskNumber) {
+		this(airport, passengerQueue, 3000, deskNumber);
+	}
 
-    /* =======================
-           ACCESSORS
-    ======================= */
+	/*
+	 * ======================= ACCESSORS =======================
+	 */
 
-    /**
-     * Output the actual desk number.
-     * @return deskNumber
-     *      Number of the desk.
-     */
-    public int getDeskNumber() {
-        return deskNumber;
-    }
+	/**
+	 * Output the actual desk number.
+	 * 
+	 * @return deskNumber Number of the desk.
+	 */
+	public int getDeskNumber() {
+		return deskNumber;
+	}
 
-    /**
-     * Output the current passenger.
-     * @return currentPassenger
-     *      Passenger being checked in.
-     */
-    public Passenger getCurrentPassenger() {
-        return currentPassenger;
-    }
+	/**
+	 * Output the current passenger.
+	 * 
+	 * @return currentPassenger Passenger being checked in.
+	 */
+	public Passenger getCurrentPassenger() {
+		return currentPassenger;
+	}
 
+	/*
+	 * ======================= METHODS =======================
+	 */
 
-     /* =======================
-             METHODS
-    ======================= */
+	/**
+	 * While the queue is not empty, the desk will accept passengers for check-in.
+	 * Each time a passenger is accepted, the desk will process it for a certain
+	 * time (processing time ± 2s).
+	 */
+	public void run() {
+		while (!passengerQueue.isEmpty()) {
+			try {
+				Passenger passengerToCheckIn = passengerQueue.acceptNewPassenger();
+				currentPassenger = passengerToCheckIn;
 
-    /**
-     * While the queue is not empty, the desk will accept passengers for check-in. Each time a passenger is accepted,
-     * the desk will process it for a certain time (processing time ± 2s).
-     */
-     public void run() {
-         while (!passengerQueue.isEmpty()) {
-             try {
-                 Passenger passengerToCheckIn = passengerQueue.acceptNewPassenger();
-                 currentPassenger = passengerToCheckIn;
+				if (currentPassenger != null) {
+					setChanged();
+					notifyObservers(1);
+					clearChanged();
 
-                 setChanged();
-                 notifyObservers(1);
-                 clearChanged();
+					// Set random baggage dimensions to passenger joining the queue
+					setRandomBaggageToPassenger(passengerToCheckIn);
+					// Log passenger accepted
+					AirportLogger.logDeskPassengerAccepted(this, passengerToCheckIn);
+					// Check-in passenger
+					checkIn(passengerToCheckIn);
+					// Creation of a random time for the check-in to happen.
+					Random random = new Random();
+					// The randomSign outputs either 1 or -1
+					int randomSign = random.nextInt(2) * 2 - 1;
+					// The random integer corresponds to 1000,2000 milliseconds
+					int randomInt = random.nextInt(3) * 1000;
+					int randomMillis = randomSign * randomInt;
+					// The thread will sleep for a duration between 1 second (3000 - 2000) and 5
+					// seconds (3000 + 2000)
+					int totalTime = processingTime + randomMillis;
+					Thread.sleep(totalTime);
 
-                 //Set random baggage dimensions to passenger joining the queue
-                 setRandomBaggageToPassenger(passengerToCheckIn);
-                 // Log passenger accepted
-                 AirportLogger.logDeskPassengerAccepted(this, passengerToCheckIn);
-                 //Check-in passenger
-                 checkIn(passengerToCheckIn);
-                 // Creation of a random time for the check-in to happen.
-                 Random random = new Random();
-                 // The randomSign outputs either 1 or -1
-                 int randomSign = random.nextInt(2) * 2 - 1;
-                 // The random integer corresponds to 1000,2000 milliseconds
-                 int randomInt = random.nextInt(3) * 1000;
-                 int randomMillis = randomSign * randomInt;
-                 // The thread will sleep for a duration between 1 second (3000 - 2000) and 5 seconds (3000 + 2000)
-                 int totalTime = processingTime + randomMillis;
-                 Thread.sleep(totalTime);
+					AirportLogger.logDeskTimeTaken(this, totalTime);
+				}
+			} catch (InterruptedException | IOException | NullDimensionException | NegativeDimensionException e) {
+				e.printStackTrace();
+			}
+		}
+		currentPassenger = null;
 
-                 AirportLogger.logDeskTimeTaken(this, totalTime);
-             }
-             catch (InterruptedException | IOException | NullDimensionException | NegativeDimensionException e) { e.printStackTrace(); }
-         }
-         currentPassenger = null;
+		setChanged();
+		notifyObservers();
+		clearChanged();
+	}
 
-         setChanged();
-         notifyObservers();
-         clearChanged();
-     }
+	/**
+	 * Method to associate random baggage dimension to every passenger in the
+	 * passenger list Dimensions are included between 1 and 200
+	 * 
+	 * @throws NegativeDimensionException Baggage dimensions should not be negative.
+	 * @throws NullDimensionException     Baggage dimensions should not be null.
+	 */
 
+	public void setRandomBaggageToPassenger(Passenger targetPassenger)
+			throws NegativeDimensionException, NullDimensionException {
+		Random random = new Random();
+		// All dimensions are integers between 20 and 200
+		int bWeight = random.nextInt(180) + 21;
+		int bLength = random.nextInt(180) + 21;
+		int bWidth = random.nextInt(180) + 21;
+		int bHeight = random.nextInt(180) + 21;
+		Baggage randomBaggage = new Baggage(bLength, bHeight, bWidth, bWeight);
+		targetPassenger.setBaggage(randomBaggage);
+	}
 
-    /**
-     * Method to associate random baggage dimension to every passenger in the passenger list
-     * Dimensions are included between 1 and 200
-     * @throws NegativeDimensionException
-     *      Baggage dimensions should not be negative.
-     * @throws NullDimensionException
-     *      Baggage dimensions should not be null.
-     */
+	/**
+	 * Method used for the checkIn button Set the check-in value to true if the
+	 * passenger last name and booking reference match. Set a baggage for a
+	 * passenger, display excess fee if applied.
+	 */
 
-    public void setRandomBaggageToPassenger(Passenger targetPassenger) throws NegativeDimensionException, NullDimensionException {
-        Random random = new Random();
-        // All dimensions are integers between 20 and 200
-        int bWeight = random.nextInt(180) + 21;
-        int bLength = random.nextInt(180) + 21;
-        int bWidth = random.nextInt(180) + 21;
-        int bHeight = random.nextInt(180) + 21;
-        Baggage randomBaggage = new Baggage(bLength, bHeight, bWidth, bWeight);
-        targetPassenger.setBaggage(randomBaggage);
-    }
+	public void checkIn(Passenger targetPassenger) {
 
-    /**
-     * Method used for the checkIn button
-     * Set the check-in value to true if the passenger last name and booking reference match.
-     * Set a baggage for a passenger, display excess fee if applied.
-     */
-
-    public void checkIn(Passenger targetPassenger) {
-
-            try {
-                String targetFlightRef = targetPassenger.getFlightReference();
-                Flight targetFlight = airport.getFlightFromRef(targetFlightRef);
-                Passenger.CheckinResult checkInResult = targetPassenger.checkIn(airport);
-                String fee = Integer.toString(targetFlight.getExcessFee());
-                switch(checkInResult){
-                    case DONE:
-                        AirportLogger.logCheckInComplete(this, targetPassenger);
-                        break;
-                    case ERR_FLIGHT_REFERENCE:
-                        AirportLogger.logCheckInErrorFlight(this, targetPassenger);
-                        break;
-                    case WARNING_ALREADY_DONE:
-                        AirportLogger.logCheckInAlreadyDone(this, targetPassenger);
-                        break;
-                    case WARNING_BAGGAGE_VOLUME:
-                        AirportLogger.logCheckInBaggageVolume(this, targetPassenger, fee);
-                        break;
-                    case WARNING_BAGGAGE_WEIGHT:
-                        AirportLogger.logCheckInBaggageWeight(this, targetPassenger, fee);
-                        break;
-                    case ERR_FLIGHT_IS_FULL:
-                        AirportLogger.logCheckInFlightFull(this, targetPassenger);
-                        break;
-                }
-            } catch (FlightNotFoundException | IOException e) {
-                e.printStackTrace();
-            }
-    }
+		try {
+			String targetFlightRef = targetPassenger.getFlightReference();
+			Flight targetFlight = airport.getFlightFromRef(targetFlightRef);
+			Passenger.CheckinResult checkInResult = targetPassenger.checkIn(airport);
+			String fee = Integer.toString(targetFlight.getExcessFee());
+			switch (checkInResult) {
+			case DONE:
+				AirportLogger.logCheckInComplete(this, targetPassenger);
+				break;
+			case ERR_FLIGHT_REFERENCE:
+				AirportLogger.logCheckInErrorFlight(this, targetPassenger);
+				break;
+			case WARNING_ALREADY_DONE:
+				AirportLogger.logCheckInAlreadyDone(this, targetPassenger);
+				break;
+			case WARNING_BAGGAGE_VOLUME:
+				AirportLogger.logCheckInBaggageVolume(this, targetPassenger, fee);
+				break;
+			case WARNING_BAGGAGE_WEIGHT:
+				AirportLogger.logCheckInBaggageWeight(this, targetPassenger, fee);
+				break;
+			case ERR_FLIGHT_IS_FULL:
+				AirportLogger.logCheckInFlightFull(this, targetPassenger);
+				break;
+			}
+		} catch (FlightNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
